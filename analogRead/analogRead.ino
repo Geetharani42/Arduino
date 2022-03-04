2aba53222a342ce3aed38618115ca32ee62a7778
Geetharani42 <72381802+Geetharani42@users.noreply.github.com>
2022-03-04 14:44:37 +0530

    Add files via upload

---
+++ analogRead/analogRead.ino
@@ -0,0 +1,26 @@
+//Pin declaration
+#define analogdata A1
+
+//Variable declaration
+int k;
+float vol;
+
+void setup() {
+  //Baudrate setting 
+  Serial.begin(9600);
+
+  //pin direction setting
+  pinMode(analogdata, INPUT);
+}
+
+void loop() {
+  //analogdata read
+  k = analogRead(analogdata);
+
+  //voltage conversion
+  vol = k * (5.0/1023.0)
+
+  //printing data in serial monitor
+  Serial.println("Analog data:"+String(k));
+  Serial.println("Voltage(V):"+String(vol));
+}
