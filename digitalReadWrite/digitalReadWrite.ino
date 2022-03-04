c5794f1b8b758b5f01ade366ae9a32532b29a963
Geetharani42 <72381802+Geetharani42@users.noreply.github.com>
2022-03-04 11:56:57 +0530

    Add files via upload

---
+++ digitalReadWrite/digitalReadWrite.ino
@@ -0,0 +1,26 @@
+//pin declaration
+#define output 3 //any digital pin
+#define input  4
+//variable declaration
+int k;
+
+void setup() {
+  //pin direction setting (input/output)
+  pinMode(output,OUTPUT);
+  pinMode(input, INPUT);
+
+  //Initial conditions set to 0
+  digitalWrite(output, LOW);
+}
+
+void loop() {
+  k = digitalRead(input) //value will be either 1 or 0
+
+  //condition
+  if(k == 1){  //put condition according to application
+    digitalWrite(output, HIGH);
+  }
+  else{   //when threshold condition fails
+    digitalWrite(output, LOW);
+  }
+}
