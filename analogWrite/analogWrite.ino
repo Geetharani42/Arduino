ae05021b0f0d8866c3e0b01f903ef304b6d323c2
Geetharani42 <72381802+Geetharani42@users.noreply.github.com>
2022-03-04 15:31:47 +0530

    Add files via upload

---
+++ analogWrite/analogWrite.ino
@@ -0,0 +1,24 @@
+//Pin declaration
+#define pwmpin 6   //choose any pwm pin for analogwrite
+
+void setup() {
+  //Serial baudrate setting
+  Serial.begin(9600);
+
+  //Pin direction setting
+  pinMode(pwmpin, OUTPUT);
+}
+
+void loop() {
+  //checking the serial data from serial monitor
+  if (Serial.available() > 0) {
+    //reading the serial data
+    int analograw =  Serial.parseInt();
+
+    //Serially printing the data
+    Serial.println("Read data:"+String(analograw));
+
+    //writing data to pwmpin
+    analogWrite(pwmpin, analograw);
+}
+}
